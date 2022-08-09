cd31bb2c79e2d00eb2c6f091df35eaaf4bd4b106
mwafrika <37586807+GloireIrenge@users.noreply.github.com>
2022-08-09 17:29:38 +0200

    Import modules into index.js

--- index.js
+++ index.js
@@ -1,125 +1,104 @@
+import luxon from './modules/luxon.js';
+import Book from './modules/add_remove_books.js';
+
+import { loadStorage } from './modules/storeBooks.js';
+
 const titleInput = document.getElementById('title');
 const authorInput = document.getElementById('author');
 const submitButton = document.getElementById('submit');
-const index = 10;
-const bookListDiv = document.getElementById('list');
-const formDiv = document.getElementById('form');
-const contactDiv = document.getElementById('contact');
-const NavList = document.getElementById('Mylist');
-const NavAdd = document.getElementById('AddBooks');
-const NavContact = document.getElementById('Mycontact');
 
-import { DateTime } from 'modules/luxon.js';
-const dateElement = document.getElementById('date');
-const date = DateTime.fromISO('2010-10-22T21:38:00');
-dateElement.textContent = date.toLocaleString(DateTime.DATETIME_MED);
+const todayDate = document.getElementById('date');
+const DTime = luxon.DateTime.now().toLocaleString(luxon.DateTime.DATETIME_MED);
 
+todayDate.innerHTML = DTime;
+const addBook = (book) => {
+  Book.addBook(book);
+};
 
+const removeBook = (bookId) => {
+  Book.removeBook(bookId);
+};
 
-
-
-
-function loadLocalStorage() {
-  if (localStorage.getItem('mybook') === null) {
-    localStorage.setItem('mybook', JSON.stringify([]));
-    const data = localStorage.getItem('mybook');
-    const bookStorage = JSON.parse(data);
-    return bookStorage;
-  }
-  const data = localStorage.getItem('mybook');
-  const bookStorage = JSON.parse(data);
-  return bookStorage;
-}
-
-let storage = loadLocalStorage();
-
-function updateLocalStorage() {
-  localStorage.setItem('mybook', JSON.stringify(storage));
-}
-
-const DisplayBook = (mybook) => {
-  const { title, author } = mybook;
+const renderBook = (book) => {
+  const { title, author } = book;
 
   const renderContainer = document.createElement('tr');
-  renderContainer.innerHTML = `
-        <td> " ${title} " by ${author} <td>
-        <button class="bg-danger text-light border-0 d-block" type="button" onclick="BookRemove('${mybook.id}')">Delete</button></td>
-  `;
+  const tdTitle = document.createElement('td');
+  const tdAuthor = document.createElement('td');
+  const tdButton = document.createElement('td');
+  tdButton.id = 'tdButton';
+  const button = document.createElement('button');
+  button.className = 'bg-danger text-light border-0 d-block';
+  button.textContent = 'Delete';
+
+  tdTitle.innerHTML = title;
+  tdAuthor.innerHTML = author;
+
+  tdButton.appendChild(button);
+  renderContainer.appendChild(tdTitle);
+  renderContainer.appendChild(tdAuthor);
+  renderContainer.appendChild(tdButton);
   return renderContainer;
 };
 
 const DisplayBooks = () => {
   const container = document.getElementById('ctn-book');
   container.innerHTML = '';
+  const storage = loadStorage();
   if (storage !== null) {
-    storage.forEach((mybook) => {
-      container.appendChild(DisplayBook(mybook));
+    storage.forEach((book) => {
+      const child = renderBook(book);
+      const button = child.children[2].firstChild;
+      button.addEventListener('click', () => {
+        removeBook(book.id);
+        DisplayBooks();
+      });
+      container.appendChild(child);
     });
   }
 };
-class MyBooks {
-  constructor(id, title, author) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-  }
-
-  static addBook(mybook) {
-    storage.push(mybook);
-    updateLocalStorage();
-    DisplayBooks();
-  }
-
-  static BookRemove(bookId) {
-    const books = storage.filter((item) => item.id !== parseInt(bookId, index));
-    storage = books;
-    updateLocalStorage();
-    DisplayBooks();
-  }
-}
-
-DisplayBooks();
-
-function addBook(mybook) {
-  MyBooks.addBook(mybook);
-}
-
-function BookRemove(bookId) {
-  MyBooks.BookRemove(bookId);
-}
-BookRemove();
 
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
-  const id = storage.length + 1;
+  const id = Math.floor(Math.random() * 10000) + 1;
   const title = titleInput.value;
   const author = authorInput.value;
-  const mybook = new MyBooks(id, title, author);
+  const book = new Book(id, title, author);
   titleInput.value = '';
   authorInput.value = '';
-  addBook(mybook);
+  addBook(book);
   DisplayBooks();
 });
 
-function constructListSection() {
+const NavList = document.getElementById('Mylist');
+const NavAdd = document.getElementById('AddBooks');
+const NavContact = document.getElementById('Mycontact');
+
+const bookListDiv = document.getElementById('list');
+const formDiv = document.getElementById('form');
+const contactDiv = document.getElementById('contact');
+
+const constructListSection = () => {
   bookListDiv.style.display = 'block';
   formDiv.style.display = 'none';
   contactDiv.style.display = 'none';
 
-  renderBooks();
-}
-function constructFormSection() {
+  DisplayBooks();
+};
+
+const constructFormSection = () => {
   bookListDiv.style.display = 'none';
   formDiv.style.display = 'block';
   contactDiv.style.display = 'none';
-}
-function constructContactSection() {
+};
+
+const constructContactSection = () => {
   bookListDiv.style.display = 'none';
   formDiv.style.display = 'none';
   contactDiv.style.display = 'block';
-}
+};
 
-function section(page) {
+const section = (page) => {
   switch (page) {
     case 0:
       constructListSection();
@@ -133,15 +112,19 @@ function section(page) {
     default:
       break;
   }
-}
+};
+
 NavList.addEventListener('click', () => {
   section(0);
 });
+
 NavAdd.addEventListener('click', () => {
   section(1);
 });
+
 NavContact.addEventListener('click', () => {
   section(2);
 });
+
 section(0);
 DisplayBooks();
